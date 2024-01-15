422bb04f5b64cc32b4d60b4e7e34565a2d9399a0
Dimitrios Dekas <dimitride@csd.auth.gr>
2024-01-15 21:38:36 +0200

    Add files via upload

---
+++ scripts/long_term_forecast/ILI_script/ETSformer.sh
@@ -0,0 +1,24 @@
+export CUDA_VISIBLE_DEVICES=1
+
+model_name=ETSformer
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/illness/ \
+  --data_path national_illness.csv \
+  --model_id ili_36_24 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 36 \
+  --label_len 18 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 2 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1
